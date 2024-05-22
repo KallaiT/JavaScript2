4edae6ce80b52e7ac91cba4424149a21c02ad1a7
TMX <a@a.com>
2024-05-22 09:09:12 +0200

    p

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-let emberek = []
+const emberek = ['Kata', 'Sára']
+const container = document.querySelector('#container')
+
+for (const ember of emberek) {
+  div = document.createElement('div')
+  div.innerText = ember
+  container.appendChild(div)
+  console.log(emberek)
+}
